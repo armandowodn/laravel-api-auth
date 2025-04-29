a3202d676e7c9004da42dfcb20b654ae81bf434d
agonkongo <armandojhe3@gmail.com>
2025-04-29 16:54:23 +0800

    v2

--- app/Common/Helper.php
+++ app/Common/Helper.php
@@ -11,6 +11,35 @@ class Helper
         return $arr;
     }
 
+    public function DBupdate_single($table = "",$arr,$whereClause,$conn = "mysql"){
+        try {
+            $set_string = "";
+            $whereClause_string = "";
+            foreach ($arr as $key => $val) {
+                $set_string .= $key." = '".$val."',";
+            }
+
+            foreach ($whereClause as $key) {
+                $aa = "";
+                foreach ($key as $j => $val) {
+                    if((count($key) - 1) == $j) {
+                        $val = '"'.$val.'"';
+                    }
+                    $aa .= $val." ";
+                }
+                $whereClause_string .= $aa." AND ";
+            }
+            
+            $set_string = rtrim($set_string, ',');
+            $whereClause_string = " WHERE ".rtrim($whereClause_string, ' AND ');
+            $query = "UPDATE ".$table." SET ".$set_string." ".$whereClause_string.";";
+            DB::connection($conn)->update($query);
+            return true;
+        }catch (\Exception $e) {
+            return false;
+        }
+    }
+
     public function ok($msg = "",$data = []){
         $data = [
             'msg'      => $msg,
@@ -39,13 +68,16 @@ class Helper
         return response()->json($data, 200);
     }
     public function error(){
+        $error_status = 500;
+        $response_message = Config::get('constants.response_message');
+        $error = $response_message[$error_status];
         $data = [
-            'msg' => 'An error occured while processing your request. Please refresh the page and try again.',
+            'msg' => $error['msg'],
             'data' => [],
             'success' => false,
-            'msgType' => 'error',
-            'msgTitle' => 'Error!'
+            'msgType' => $error['msgType'],
+            'msgTitle' => $error['msgTitle'],
         ];
-        return response()->json($data, 500);
+        return response()->json($data, $error_status);
     }
 }
